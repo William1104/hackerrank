f9424566c1ad6454a7fa76afca7c275985530d13
william <william>
2015-11-27 02:05:57 +0800

    implement solution for  staircase project

--- Staircase/src/main/java/Solution.java
+++ Staircase/src/main/java/Solution.java
@@ -4,7 +4,19 @@
 public class Solution {
     public static final class Resolver {
         public static String[] resolve(int size) {
-            return new String[]{};
+            final String[] outputs = new String[size];
+            for (int i = 0; i < size; i++) {
+                final StringBuilder sb = new StringBuilder();
+                for (int j = 0; j < size - (i + 1); j++) {
+                    sb.append(' ');
+                }
+                for (int j = (size - (i + 1)); j < size; j++) {
+                    sb.append('#');
+                }
+                outputs[i] = sb.toString();
+            }
+
+            return outputs;
         }
     }
 
