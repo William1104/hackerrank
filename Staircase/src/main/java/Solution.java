c78b2f0d1679f866c33ce8806893fbc6259a0ed7
william <william>
2015-11-27 02:09:44 +0800

    implement input/output logic for  hackerrank submission

--- Staircase/src/main/java/Solution.java
+++ Staircase/src/main/java/Solution.java
@@ -1,7 +1,21 @@
+import java.util.Scanner;
+import java.util.stream.Stream;
+
 /**
  * Created by william on 11/27/2015.
  */
 public class Solution {
+    public static void main(String[] args) {
+
+        try (Scanner in = new Scanner(System.in)) {
+            final int size = Integer.valueOf(in.nextLine());
+
+            final String[] outputs = Resolver.resolve(size);
+
+            Stream.of(outputs).forEach(System.out::println);
+        }
+    }
+
     public static final class Resolver {
         public static String[] resolve(int size) {
             final String[] outputs = new String[size];
