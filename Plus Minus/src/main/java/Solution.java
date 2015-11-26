392a534692c2a3dce2fca779606de621f8ac019b
william <william>
2015-11-27 01:43:57 +0800

    implement plus minus solution

--- Plus Minus/src/main/java/Solution.java
+++ Plus Minus/src/main/java/Solution.java
@@ -1,3 +1,5 @@
+import java.util.stream.IntStream;
+
 /**
  * Created by william on 11/27/2015.
  */
@@ -5,7 +7,16 @@ public class Solution {
 
     public static final class Resolver {
         public static double[] resolve(int size, int[] nums) {
-            return new double[]{0, 0, 0};
+
+            long positiveNum = IntStream.of(nums).filter(num -> num > 0).count();
+            long negativeNum = IntStream.of(nums).filter(num -> num < 0).count();
+            long zeoNum = IntStream.of(nums).filter(num -> num == 0).count();
+
+            return new double[]{
+                    1.0 * positiveNum / size,
+                    1.0 * negativeNum / size,
+                    1.0 * zeoNum / size
+            };
         }
     }
 }
