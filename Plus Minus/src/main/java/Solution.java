610c56245af31622b2c94f44acd47d7382428fb7
william <william>
2015-11-27 01:51:29 +0800

    add input/output logic to run the solution on hackerrank

--- Plus Minus/src/main/java/Solution.java
+++ Plus Minus/src/main/java/Solution.java
@@ -1,3 +1,6 @@
+import java.text.DecimalFormat;
+import java.util.Arrays;
+import java.util.Scanner;
 import java.util.stream.IntStream;
 
 /**
@@ -5,12 +8,29 @@ import java.util.stream.IntStream;
  */
 public class Solution {
 
+    public static void main(String[] args) {
+
+        try (Scanner in = new Scanner(System.in)) {
+            final int size = Integer.valueOf(in.nextLine());
+
+            final int[] nums = Arrays
+                    .stream(in.nextLine().split(" "))
+                    .mapToInt(Integer::valueOf).toArray();
+
+            final double[] outputs = Resolver.resolve(size, nums);
+            final DecimalFormat df = new DecimalFormat("#.######");
+            System.out.println(df.format(outputs[0]));
+            System.out.println(df.format(outputs[1]));
+            System.out.println(df.format(outputs[2]));
+        }
+    }
+
     public static final class Resolver {
         public static double[] resolve(int size, int[] nums) {
 
-            long positiveNum = IntStream.of(nums).filter(num -> num > 0).count();
-            long negativeNum = IntStream.of(nums).filter(num -> num < 0).count();
-            long zeoNum = IntStream.of(nums).filter(num -> num == 0).count();
+            final long positiveNum = IntStream.of(nums).filter(num -> num > 0).count();
+            final long negativeNum = IntStream.of(nums).filter(num -> num < 0).count();
+            final long zeoNum = IntStream.of(nums).filter(num -> num == 0).count();
 
             return new double[]{
                     1.0 * positiveNum / size,
