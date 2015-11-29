3efa8ebb23176b495d629ca8330bf77c5196bedd
william1104 <william1104@gmail.com>
2015-11-30 01:06:43 +0800

    update caesar-cipher with solution implementation

--- caesar-cipher/src/main/java/Solution.java
+++ caesar-cipher/src/main/java/Solution.java
@@ -1,9 +1,18 @@
+import java.util.Scanner;
+
 /**
  * Created by william on 11/27/2015.
  */
 public class Solution {
     public static void main(String[] args) {
+        try (final Scanner scanner = new Scanner(System.in)) {
+            int numChar = scanner.nextInt();
+            String input = scanner.nextLine(); // read an empty line first.
+            input = scanner.nextLine();
 
+            int shift = scanner.nextInt();
+            System.out.println(Resolver.resolve(input, shift));
+        }
     }
 
     public static final class Resolver {
@@ -11,8 +20,18 @@ public class Solution {
                 final String input,
                 final int shift) {
 
-            throw new UnsupportedOperationException();
-
+            return input.codePoints()
+                    .map(c -> {
+                        if (c >= 'a' && c <= 'z') {
+                            return (((c + shift) - 'a') % 26) + 'a';
+                        } else if (c >= 'A' && c <= 'Z') {
+                            return (((c + shift) - 'A') % 26) + 'A';
+                        } else {
+                            return c;
+                        }
+                    })
+                    .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                    .toString();
         }
     }
 
