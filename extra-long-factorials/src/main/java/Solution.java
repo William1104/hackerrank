968df7cb354f6c7a36bdd3ce330f2b4d3686cb3e
william1104 <william1104@gmail.com>
2015-11-29 23:18:36 +0800

    update problem 'extra-long-factorials' with solution implementation

--- extra-long-factorials/src/main/java/Solution.java
+++ extra-long-factorials/src/main/java/Solution.java
@@ -1,17 +1,26 @@
 import java.math.BigInteger;
+import java.util.Scanner;
+import java.util.stream.LongStream;
 
 /**
  * Created by william on 11/27/2015.
  */
 public class Solution {
     public static void main(String[] args) {
+        try (final Scanner scanner = new Scanner(System.in)) {
+            final int n = scanner.nextInt();
+            System.out.println(Resolver.resolve(n));
+        }
 
     }
 
     public static final class Resolver {
         public static BigInteger resolve(int input) {
-            throw new UnsupportedOperationException();
-
+            return LongStream.rangeClosed(1, input)
+                    .parallel()
+                    .mapToObj(BigInteger::valueOf)
+                    .reduce(BigInteger::multiply)
+                    .orElse(BigInteger.ONE);
         }
     }
 }
