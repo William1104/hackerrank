20ae921efb0f58c032cd5f800657a77e07a62b0f
william1104 <william1104@gmail.com>
2015-11-29 14:13:47 +0800

    add the problem (service-lane) with testcase

---
+++ service-lane/src/main/java/Solution.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+/**
+ * Created by william on 11/27/2015.
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static final class Resolver {
+        private final int lengthOfFreeway;
+        private final int[] width;
+
+        public Resolver(final int lengthOfFreeway, final int[] width) {
+            super();
+            this.lengthOfFreeway = lengthOfFreeway;
+            this.width = Arrays.copyOf(width, lengthOfFreeway);
+        }
+
+        public int resolve(int start, int end) {
+            throw new UnsupportedOperationException();
+        }
+    }
+}
---
+++ service-lane/src/test/java/TestResolver.java
@@ -0,0 +1,39 @@
+import org.hamcrest.Matchers;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+
+/**
+ * Created by william on 11/27/2015.
+ */
+@RunWith(Parameterized.class)
+public class TestResolver {
+
+    @Parameterized.Parameter(value = 1)
+    public int start;
+    @Parameterized.Parameter(value = 2)
+    public int end;
+    @Parameterized.Parameter(value = 3)
+    public int expectedOutput;
+    @Parameterized.Parameter(value = 0)
+    private Solution.Resolver resolver;
+
+    @Parameterized.Parameters(name = "{index}: start[{1}], end[{2}] -> result[{3}]")
+    public static Iterable<Object[]> data() {
+        return Arrays.asList(
+                new Object[]{new Solution.Resolver(8, new int[]{2, 3, 1, 2, 3, 2, 3, 3}), 0, 3, 1},
+                new Object[]{new Solution.Resolver(8, new int[]{2, 3, 1, 2, 3, 2, 3, 3}), 4, 6, 2},
+                new Object[]{new Solution.Resolver(8, new int[]{2, 3, 1, 2, 3, 2, 3, 3}), 6, 7, 3},
+                new Object[]{new Solution.Resolver(8, new int[]{2, 3, 1, 2, 3, 2, 3, 3}), 3, 5, 2},
+                new Object[]{new Solution.Resolver(8, new int[]{2, 3, 1, 2, 3, 2, 3, 3}), 0, 7, 1});
+    }
+
+    @Test
+    public void testResolve() {
+        final int output = resolver.resolve(start, end);
+        Assert.assertThat(output, Matchers.equalTo(expectedOutput));
+    }
+}
