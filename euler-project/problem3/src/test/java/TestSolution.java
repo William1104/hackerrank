570797345fa8cf1d1b63d5f6046af077b7bd66a1
williamwong <william1104@gmail.com>
2016-01-20 00:40:23 +0800

    add soluton of problem3

---
+++ euler-project/problem3/src/test/java/TestSolution.java
@@ -0,0 +1,39 @@
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+
+import static org.junit.runners.Parameterized.Parameter;
+import static org.junit.runners.Parameterized.Parameters;
+
+/**
+ * Created by williamwong on 17/1/2016.
+ */
+
+@RunWith(value = Parameterized.class)
+public class TestSolution {
+
+    @Parameter(0)
+    public long input;
+
+    @Parameter(1)
+    public long expectedOutput;
+
+    @Parameters(name = "{index}: {0} -> {1}")
+    public static Iterable<Object[]> data() {
+        return Arrays.asList(
+                new Object[]{10, 5},
+                new Object[]{17, 17},
+                new Object[]{25, 5},
+                new Object[]{600851475143L, 6857}
+        );
+    }
+
+    @Test
+    public void test() {
+        MatcherAssert.assertThat(Solution.solve(input), Matchers.equalTo(expectedOutput));
+    }
+}
